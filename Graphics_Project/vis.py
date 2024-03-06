cbe4352ee538500077feb09049dca702bcf8b8c5
Andrew Yang <yangatang129@gmail.com>
2024-03-07 08:41:34 +1000

    start!

---
+++ Graphics_Project/vis.py
@@ -0,0 +1,11 @@
+from matplotlib import pyplot as plt
+
+from mpl_toolkits.mplot3d import axes3d
+
+from vedo import *
+
+#mesh = Mesh("3d_models/USS enterprise gray scale.obj",)
+#mesh = Mesh("3d_models/12221_Cat_v1_l3.obj",)
+mesh = Mesh("3d_models/kibble-head-kled.stl",)
+
+mesh.show()
